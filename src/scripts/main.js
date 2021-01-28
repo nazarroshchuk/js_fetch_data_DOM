09edbee2f7d159144e27877cda0b7a7904db528e
Nazarii <nazarrishchuk@gmail.com>
2021-01-28 19:13:22 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write your code here
+const listUrl = 'https://mate-academy.github.io/phone-catalogue-static/api';
+const phonesWithDetails = [];
+
+const request = (url) => {
+  return fetch(`${listUrl}${url}`)
+    .then(response => {
+      if (!response.ok) {
+        // eslint-disable-next-line prefer-promise-reject-errors
+        setTimeout(() => Promise.reject('Something is wrong'), 5000);
+      }
+
+      return response.json();
+    });
+};
+
+const getPhones = (url) => request(url);
+
+function getPhonesDetails(arr) {
+  for (const el of arr) {
+    getPhones(`/phones/${el.id}.json`)
+      .then(result => phonesWithDetails.push(result));
+  }
+
+  return arr;
+};
+
+function createList(arr) {
+  const list = document.createElement('ul');
+
+  for (const el of arr) {
+    const li = document.createElement('li');
+
+    li.textContent = el.name;
+    list.append(li);
+  }
+  document.querySelector('body').append(list);
+
+  return arr;
+}
+
+getPhones('/phones.json')
+  .then(result => getPhonesDetails(result))
+  .then(result => createList(result))
+  .catch();
